1b32a48ed8148049b0b1471c86f2c4205c31e9cc
Neosh <neosh11@gmail.com>
2024-08-11 15:29:36 +1000

    stage 2

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -11,9 +11,6 @@ import (
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
 	if unicode.IsDigit(rune(bencodedString[0])) {
 		var firstColonIndex int
@@ -24,17 +21,22 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 				break
 			}
 		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
+		length, err := strconv.Atoi(bencodedString[:firstColonIndex])
 		if err != nil {
 			return "", err
 		}
-
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
+	} else if rune(bencodedString[0]) == 'i' {
+		var endIndex int
+		for i := 0; i < len(bencodedString); i++ {
+			if bencodedString[i] == 'e' {
+				endIndex = i
+				break
+			}
+		}
+		return strconv.Atoi(bencodedString[1:endIndex])
 	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+		return "", fmt.Errorf("Only strings, ints are supported at the moment")
 	}
 }
 
@@ -42,7 +44,6 @@ func main() {
 	command := os.Args[1]
 	if command == "decode" {
 		bencodedValue := os.Args[2]
-
 		decoded, err := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
